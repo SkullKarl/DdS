9f4297080bc6a0b23753cf77f3e030a20f1ecedf
Xaiwu <neclxas3311@gmail.com>
2025-06-01 21:52:34 -0400

    Agrego un gitignore para la base de datos

--- src/api/firebaseConfig.ts
+++ src/api/firebaseConfig.ts
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBu5j3izIQDglHom9OyBWevWfG5Y_2NxWc",
-  authDomain: "softwaredesign-9025c.firebaseapp.com",
-  projectId: "softwaredesign-9025c",
-  storageBucket: "softwaredesign-9025c.firebasestorage.app",
-  messagingSenderId: "780784228827",
-  appId: "1:780784228827:web:32e240ea3254265d5b2ffe"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-
 export default app;
