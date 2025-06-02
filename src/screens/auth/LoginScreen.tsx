f6dcf318147440b9f9139f13fe34a3270aa21f36
gustavo <gustavo.gonzalez8190@gmail.com>
2025-06-01 22:23:55 -0400

    Probando algunos componentes

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,9 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function LoginScreen({ navigation }: any) {
   return (
-    <View style={styles.container}>
+    <ImageBackground
+    source={{uri: 'https://imgs.search.brave.com/fNHuNsDQIoveTNYT4ODt3rfRmP6uJun8mh1YSbVNewU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTgx/OTc1MDk4MS92ZWN0/b3IvZ3BzLWdyYXBo/aWMtdG91cmlzdC1t/YXAtb2YtdGVycml0/b3J5LXNtYXJ0cGhv/bmUtbWFwLWFwcGxp/Y2F0aW9uLWFwcC1z/ZWFyY2gtbWFwLW5h/dmlnYXRpb24uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPVFR/MGc2WEdTY1RQQm9r/LWk5SlZ2U2ZrV0J6/Zjdxa3h0Z0xCQXUy/ZEY1REU9'}}
+    style={styles.container}>
+      <StatusBar
+      style="dark"/>
       <Text style={styles.title}>Pantalla de Login</Text>
       
       <Button 
@@ -31,7 +36,7 @@ export default function LoginScreen({ navigation }: any) {
         title="Entrar como Admin"
         onPress={() => navigation.navigate('HomeAdmin')}
       />
-    </View>
+    </ImageBackground>
   );
 }
 
