f3616380b21bd8370399b9850cf0b467a1e4dc7e
gustavo <gustavo.gonzalez8190@gmail.com>
2025-06-02 19:47:00 -0400

    Map View agregada a la pantalla del despachador

--- Frontend/src/screens/dispatcher/HomeDispatcherScreen.tsx
+++ Frontend/src/screens/dispatcher/HomeDispatcherScreen.tsx
@@ -1,11 +1,26 @@
 // HomeDriverScreen.tsx
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function HomeDispatcherScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Bienvenido Despachador</Text>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+});
